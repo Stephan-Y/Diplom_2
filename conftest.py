5b50bfce384c9e194d7998bc55e2b8275ad757fa
Stepan_Y <stepan.yakovlev.b@yandex.ru>
2024-05-15 17:32:30 +0300

    conftest

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
-import allure
+import pytest
 import requests
 from data import *
-import json
+from helpers import UserTestData
 
+
+@pytest.fixture
+def gen_new_user_data():
+    response = requests.post(Url.CREATE_USER, data=UserTestData.gen_new_user())
+    yield response
+    token = str(response.json()['accessToken'])
+    requests.delete(Url.UPDATE_USER, headers={'Authorization': token})
