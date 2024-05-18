6150c6bd2e11db0ac419a871261f93266c7bb35b
Stepan_Y <stepan.yakovlev.b@yandex.ru>
2024-05-18 17:28:55 +0300

    TestCreateOrder

--- data.py
+++ data.py
@@ -24,7 +24,7 @@ class Error:
 
 
 class Ingredient:
-    METEOR_BURGER = {"ingredients": ["60d3b41abdacab0026a733c6", "609646e4dc916e00276b2870"]}
+    LIGHT_BURGER = {"ingredients": ["61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6f"]}
     INCORRECT_BURGER = {"ingredients": ["61c0c5INCORRECT1bdaaa6d", "61c0c5a71INCORRECTdaaa72"]}
     EMPTY_BURGER = {"ingredients": []}
 
